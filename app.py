16837a43baa96a91436df963b79411c0018ba5a9
JuanGM07 <juanglezm3@gmail.com>
2025-03-19 18:09:13 +0100

    web finished

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flask import Flask, render_template, request
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from collections import Counter
@@ -9,6 +8,9 @@ import os
 from serpOpinions import scrap_urls, scrape_all_opinions
 from googleOpinions import get_place_id, obtener_resenas
 from NLPAnalysis import sentiment_analysis
+from datetime import datetime
+from flask import Flask, request, render_template, redirect, url_for, session
+import secrets
 
 load_dotenv()
 
@@ -17,12 +19,17 @@ app = Flask(__name__)
 # Google API KEY
 apiKey = os.getenv("API_KEY")
 serpApiKey = os.getenv("SERP_API_KEY")
+app.secret_key = os.getenv('SECRET_KEY')  # Leer la clave desde el archivo .env
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         nombreNegocio = request.form['business_name']
-        
+
+        # Llamar a la función de control de acceso
+        if not controlar_acceso():
+            return render_template('error.html', message="Ya has utilizado la herramienta hoy.")
+
         # Obtener opiniones
         place_id = get_place_id(nombreNegocio, apiKey)
         resenas_google = obtener_resenas(apiKey, place_id)
@@ -40,8 +47,17 @@ def index():
         img_negativas = generar_wordcloud(negativas, "Reds")
         
         return render_template('dashboard.html', business=nombreNegocio, img_positivas=img_positivas, img_negativas=img_negativas)
-    
+
     return render_template('index.html')
+
+def controlar_acceso():
+    if session.get('usos_hoje') == 1:
+        # Si ya se ha utilizado hoy, redirige a otra página o muestra un mensaje
+        return False
+    else:
+        # Si no se ha usado, establece la variable en la sesión
+        session['usos_hoje'] = 1
+        return True
 
 def generar_wordcloud(palabras, color):
     """ Genera una imagen de nube de palabras y la convierte a base64."""
